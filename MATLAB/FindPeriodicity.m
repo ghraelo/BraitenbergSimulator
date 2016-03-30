94c07877db616c43a01e49d4f77e4b11eae737e3
Jack Cosslett <jc21g13@soton.ac.uk>
2016-03-30 14:06:41 +0100

    remove debug plots

--- MATLAB/FindPeriodicity.m
+++ MATLAB/FindPeriodicity.m
@@ -1,26 +1,12 @@
-function [period] = FindPeriodicity(x,t)
+function [period] = FindPeriodicity(x)
 %FindPeriodicity - detect periodicity in data
 %   http://www.sciencedirect.com/science/article/pii/S0959152410000600
-
-figure;
-subplot(3,1,1);
-plot(t,x);
-title('x');
-
 y = dct(x);
-
-subplot(3,1,2);
-plot(t,y);
-title('dct(x)');
 
 %remove noise
 sigma = std(y);
 sea_level = 3 * sigma;
 y(abs(y) < sea_level) = 0;
-
-subplot(3,1,3);
-plot(t,y);
-title('thresholded dct(x)');
 
 %extract different frequency components
 
@@ -57,8 +43,6 @@ for n = 1 : size(indexMatrix,1)
     y_i(indexMatrix(n,end):end) = 0;
     
     x_i = idct(y_i);
-    figure;
-    plot(t,x_i);
     
     %zero crossings    
     Z_i = zerocross(x_i);
@@ -67,7 +51,7 @@ for n = 1 : size(indexMatrix,1)
     for m = 2 : length(Z_i)
         T_i = [T_i 2*(Z_i(m) - Z_i(m-1))];
     end
-    T_i
+
     meanT_i = mean(T_i);
     sigmaT_i = std(T_i);
     
--- MATLAB/test_periodicity.m
+++ MATLAB/test_periodicity.m
@@ -5,7 +5,7 @@ t = [0:1/sr:2];
 x = sin(2*pi*f*t)+sin(2*pi*f2*t);
 %x = awgn(x,4);
 
-y = FindPeriodicity(x,t);
+y = FindPeriodicity(x);
 
 y = y/sr;
 
